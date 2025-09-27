d01ec1c0eeb98fa3dcc804ca38811f0c614771ee
Ruben Dominguez <argenis.dominguez@hotmail.com>
2025-09-26 23:12:04 -0400

    add branch name check mjs

--- branch.mjs
+++ branch.mjs
@@ -1,7 +1,18 @@
 import { execSync } from 'node:child_process';
 
 (() => {
-  const regex = new RegExp(/^(main|feature\/|hotfix\/|bugfix\/|enhancement\/)/);
+  const regex = new RegExp(/^(feature\/|hotfix\/|bugfix\/|enhancement\/)/);
   const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-  console.log(regex.test(branch));
+  const pass = regex.test(branch);
+  console.log(
+    'Check branch name: ' + branch + ' :: ' + (pass ? 'PASS' : 'FAIL'),
+  );
+
+  if (!pass) {
+    console.log(
+      'Branch name must start with feature/, hotfix/, bugfix/ or enhancement/',
+    );
+    console.log('Use "git branch -M <new-branch-name>" to rename the branch');
+    process.exit(1);
+  }
 })();
